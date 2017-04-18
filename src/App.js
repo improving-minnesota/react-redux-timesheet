f11922d32f3cfc26c7fbbda205c6a7a4d77de236
scottbock <l.scott.bock@gmail.com>
2017-04-18 10:47:07 -0500

    initial react-router inclusion

--- src/App.js
+++ src/App.js
@@ -2,20 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import PeopleContainer from './components/PeopleContainer';
-
+import {BrowserRouter, Route, Link} from 'react-router-dom';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <PeopleContainer />
-      </div>
+        <BrowserRouter>
+          <div className="App">
+            <div className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h2>Welcome to React</h2>
+            </div>
+            <Link to="/"> Home</Link>
+            <Link to="/people"> People</Link>
+            <Route exact={true} path={"/"} render={() => (
+              <p className="App-intro">
+                To get started, edit <code>src/App.js</code> and save to reload.
+              </p>
+            )}/>
+            <Route path={"/people"} component={PeopleContainer}/>
+          </div>
+        </BrowserRouter>
     );
   }
 }
