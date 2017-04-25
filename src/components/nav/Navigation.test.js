9e481428e143a6de0f9425a248ede2a82ebd6c23
scottbock <l.scott.bock@gmail.com>
2017-04-25 09:55:35 -0500

    import browser router in nav test

--- src/components/nav/Navigation.test.js
+++ src/components/nav/Navigation.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navigation from './Navigation';
 import ReactTestUtils from 'react-dom/test-utils';
-
+import {BrowserRouter} from 'react-router-dom';
 
 describe('Navigation Component: ', function () {
 
