b10ff236ed6428a6504a4fb40c120125666c8b10
Jeff Sheets <jeffsheets@Jeffs-MacBook-Pro-3.local>
2017-05-02 10:37:20 -0500

    Placeholder for employee reducer

--- src/reducers/employee-reducer.js
+++ src/reducers/employee-reducer.js
@@ -1,13 +1,1 @@
-import * as ProjectActionTypes from '../actions/ProjectActionTypes';
-
-export default (state = {projects: [], project: {}}, action) => {
-  switch (action.type) {
-    case ProjectActionTypes.LIST:
-      return Object.assign({}, state, {projects: action.projects});
-    case ProjectActionTypes.GET:
-      return Object.assign({}, state, {project: action.project});
-
-    default:
-      return state;
-  }
-}
+//Placeholder for employee-reducer
