0a156ac41ac1e74fcfb5026a862b2c7e6cf968cd
scottbock <l.scott.bock@gmail.com>
2017-05-04 10:42:08 -0500

    Simple Project CRUD Tests

--- src/components/projects/ProjectsCreate.test.js
+++ src/components/projects/ProjectsCreate.test.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import Projects from './Projects';
+import ProjectsCreate from './ProjectsCreate';
 import ReactTestUtils from 'react-dom/test-utils';
 import configureStore from '../../store/configure-store';
 import {BrowserRouter} from 'react-router-dom';
 
-describe('Projects Component: ', function () {
+describe('Projects Create Component: ', function () {
 
   let projects;
   const mockStore = configureStore();
 
   beforeEach(() =>{
-    projects = ReactTestUtils.renderIntoDocument(<BrowserRouter><Projects store={mockStore}/></BrowserRouter>);
+    projects = ReactTestUtils.renderIntoDocument(<BrowserRouter><ProjectsCreate store={mockStore}/></BrowserRouter>);
   });
 
-  it('should instantiate the Project Component', function () {
+  it('should instantiate the Projects Create Component', function () {
     expect(ReactTestUtils.isCompositeComponent(projects)).toBe(true);
   });
 
