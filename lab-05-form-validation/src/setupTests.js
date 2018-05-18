601914709e046633a58fc1999eeba9b195c67423
Travis Martensen <travis.martensen@gmail.com>
2018-05-18 10:17:58 -0500

    Updated all packages via `yarn upgrade-interactive --latest`. Added adapter and proper setup for `enzyme` in setupTests.js. Updated snapshots that broke with new version change.

--- lab-05-form-validation/src/setupTests.js
+++ lab-05-form-validation/src/setupTests.js
@@ -2,3 +2,9 @@
 //See: https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/template/README.md#initializing-test-environment
 
 import 'jest-enzyme';
+import { configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+configure({
+  adapter: new Adapter()
+})
