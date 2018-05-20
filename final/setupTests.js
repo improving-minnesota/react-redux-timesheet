54e65d514990c6c327c650bada10628790dc7af1
Dustin Schau <dustinschau@gmail.com>
2018-05-20 15:03:04 -0500

    chore: update dependencies

---
+++ final/setupTests.js
@@ -0,0 +1,11 @@
+console.log('hello');
+//Special create-react-app convention file to setup tests
+//See: https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/template/README.md#initializing-test-environment
+
+import 'jest-enzyme';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({
+  adapter: new Adapter()
+});
