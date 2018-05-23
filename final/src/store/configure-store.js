4ff20e33986b290a7348cf07863aaa79f2d8f74b
Travis Martensen <travis.martensen@gmail.com>
2018-05-23 12:48:01 -0500

    Added setup for Redux Devtools on store creation.

--- final/src/store/configure-store.js
+++ final/src/store/configure-store.js
@@ -1,7 +1,10 @@
 import rootReducer from '../reducers';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 export default (initialState = { projects: [] }) => {
-  return createStore(rootReducer, initialState, applyMiddleware(thunk));
+  const composeEnhancers = (process.env.NODE_ENV === 'development')
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : compose
+  return createStore(rootReducer, initialState, composeEnhancers(applyMiddleware(thunk)));
 };
