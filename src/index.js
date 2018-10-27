408b340ae050a616997b1eb73b39b9763da17d6c
Mike Plummer <mike-plummer@users.noreply.github.com>
2018-10-27 08:11:05 -0500

    Updating labs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import configureStore from './store/configure-store';
 import './index.css';
 import 'semantic-ui-css/semantic.min.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
