70e67634ce5b488a0e3c95e803ccdfd670a43dc4
Mike Plummer <mike-plummer@users.noreply.github.com>
2018-12-18 19:53:39 -0600

    Updating labs

--- src/employees/EmployeeRow.js
+++ src/employees/EmployeeRow.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Button, Table } from 'semantic-ui-react';
+import { withRouter } from 'react-router';
 
 class EmployeeRow extends React.Component {
   handleClick = () => {
@@ -13,11 +14,22 @@ class EmployeeRow extends React.Component {
     }
   };
 
+  showDetail = () => {
+    const { history, employee } = this.props;
+
+    if (employee.deleted) {
+      console.log('You cannot edit a deleted employee.');
+      return;
+    }
+
+    history.push(`/employees/detail/${employee._id}`);
+  };
+
   render() {
     const { employee } = this.props;
 
     return (
-      <Table.Row negative={employee.deleted}>
+      <Table.Row negative={employee.deleted} onClick={this.showDetail}>
         <Table.Cell>{employee.username}</Table.Cell>
         <Table.Cell>{employee.email}</Table.Cell>
         <Table.Cell>{employee.firstName}</Table.Cell>
@@ -39,4 +51,4 @@ EmployeeRow.propTypes = {
   onRestore: PropTypes.func.isRequired
 };
 
-export default EmployeeRow;
+export default withRouter(EmployeeRow);
