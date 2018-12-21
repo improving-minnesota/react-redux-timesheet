0f79e098a19420ed6770794b2553c678abe7c2ff
Mike Plummer <mike-plummer@users.noreply.github.com>
2018-12-21 10:42:42 -0600

    Switch back to react-bootstrap

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import 'semantic-ui-css/semantic.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
