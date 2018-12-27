d7d6f021e2bcb29e9917e92483c6838ffc26967f
Mike Plummer <mike-plummer@users.noreply.github.com>
2018-12-27 10:48:17 -0600

    Update labs

--- src/form/FieldWrapper.js
+++ src/form/FieldWrapper.js
@@ -1,26 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Field } from 'formik';
 import { FormControl, FormGroup } from 'react-bootstrap';
 import ControlLabel from 'react-bootstrap/es/ControlLabel';
 
 const FieldWrapper = ({ name, label, invalid, children, component, ...rest }) => (
-  <div className="field">
-    {label && <ControlLabel>{label}</ControlLabel>}
-    <Field
-      name={name}
-      render={({ field  }) => (
-        <FormGroup validationState={invalid ? 'error' : null}>
-          <FormControl {...rest} {...field} componentClass={component} >
-            {children}
-          </FormControl>
-        </FormGroup>
-      )}
-    />
-    {invalid && (
-      <small style={{ color: 'red' }}>{invalid}</small>
+  <Field
+    name={name}
+    render={({ field  }) => (
+      <FormGroup validationState={invalid ? 'error' : null}>
+        <ControlLabel>{label}</ControlLabel>
+        <FormControl {...rest} {...field} componentClass={component} >
+          {children}
+        </FormControl>
+        {invalid && (
+          <small style={{ color: 'red' }}>{invalid}</small>
+        )}
+      </FormGroup>
     )}
-  </div>
+  />
 );
+
+FieldWrapper.propTypes = {
+  label: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  invalid: PropTypes.string,
+  component: PropTypes.oneOf(['input', 'select'])
+};
 
 export {
   FieldWrapper
