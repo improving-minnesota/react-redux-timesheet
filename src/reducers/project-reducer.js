c5ed4d1e20a4e9e8a5cc9deb8baf4cf3a1ae064a
Mike Plummer <mike-plummer@users.noreply.github.com>
2018-12-31 12:50:13 -0600

    Updating labs

--- src/reducers/project-reducer.js
+++ src/reducers/project-reducer.js
@@ -1,12 +1,17 @@
 import * as ProjectActionTypes from '../actions/ProjectActionTypes';
+import * as TimesheetActionTypes from '../actions/TimesheetActionTypes';
 
-export default (state = { projects: [], project: {} }, action) => {
+export default (state = { data: [] }, action) => {
   switch (action.type) {
     case ProjectActionTypes.LIST:
-      return Object.assign({}, state, { projects: action.projects });
-    case ProjectActionTypes.GET:
-      return Object.assign({}, state, { project: action.project });
-
+      return { ...state, data: action.projects };
+    case TimesheetActionTypes.GET:
+      const updatedItem = action.project;
+      const index = state.data.findIndex(d => d._id === updatedItem._id);
+      if (index >= 0 ) {
+        return { ...state, data: [...state.data].splice(index, 1, updatedItem) };
+      }
+      return { ...state, data: [...state.data, updatedItem] };
     default:
       return state;
   }
--- src/reducers/timesheet-reducer.js
+++ src/reducers/timesheet-reducer.js
@@ -1,12 +1,16 @@
 import * as TimesheetActionTypes from '../actions/TimesheetActionTypes';
 
-export default (state = { timesheets: [], timesheet: {} }, action) => {
+export default (state = { data: [] }, action) => {
   switch (action.type) {
     case TimesheetActionTypes.LIST:
-      return Object.assign({}, state, { timesheets: action.timesheets });
+      return { ...state, data: action.timesheets };
     case TimesheetActionTypes.GET:
-      return Object.assign({}, state, { timesheet: action.timesheet });
-
+      const updatedItem = action.timesheet;
+      const index = state.data.findIndex(d => d._id === updatedItem._id);
+      if (index >= 0 ) {
+        return { ...state, data: [...state.data].splice(index, 1, updatedItem) };
+      }
+      return { ...state, data: [...state.data, updatedItem] };
     default:
       return state;
   }
