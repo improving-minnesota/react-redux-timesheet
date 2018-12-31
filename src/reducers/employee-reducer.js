02d4d885e86d8251fab9a444153d11109afceed9
Mike Plummer <mike-plummer@users.noreply.github.com>
2018-12-31 12:55:29 -0600

    Updating labs

--- src/reducers/employee-reducer.js
+++ src/reducers/employee-reducer.js
@@ -1,10 +1,16 @@
 import * as EmployeeActionTypes from '../actions/EmployeeActionTypes';
 
-export default (state = { employees: [] }, action) => {
+export default (state = { data: [] }, action) => {
   switch (action.type) {
     case EmployeeActionTypes.LIST:
-      return Object.assign({}, state, { employees: action.employees });
-
+      return { ...state, data: action.employees };
+    case EmployeeActionTypes.GET:
+      const updatedItem = action.timesheet;
+      const index = state.data.findIndex(d => d._id === updatedItem._id);
+      if (index >= 0 ) {
+        return { ...state, data: [...state.data].splice(index, 1, updatedItem) };
+      }
+      return { ...state, data: [...state.data, updatedItem] };
     default:
       return state;
   }
