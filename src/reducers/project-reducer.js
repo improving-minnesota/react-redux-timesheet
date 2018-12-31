7c3a8b530f2cf464f34120eece89261fcce08439
Mike Plummer <mike-plummer@users.noreply.github.com>
2018-12-31 12:53:59 -0600

    Updating labs

--- src/reducers/project-reducer.js
+++ src/reducers/project-reducer.js
@@ -1,11 +1,10 @@
 import * as ProjectActionTypes from '../actions/ProjectActionTypes';
-import * as TimesheetActionTypes from '../actions/TimesheetActionTypes';
 
 export default (state = { data: [] }, action) => {
   switch (action.type) {
     case ProjectActionTypes.LIST:
       return { ...state, data: action.projects };
-    case TimesheetActionTypes.GET:
+    case ProjectActionTypes.GET:
       const updatedItem = action.project;
       const index = state.data.findIndex(d => d._id === updatedItem._id);
       if (index >= 0 ) {
