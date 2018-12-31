bbbd5bd0d3b2d04a5b40fa5d8c68b600f6969c7b
Mike Plummer <mike-plummer@users.noreply.github.com>
2018-12-31 14:26:08 -0600

    Updating labs

--- src/employees/EmployeeDetail.test.js
+++ src/employees/EmployeeDetail.test.js
@@ -1,1 +1,9 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import EmployeeDetail from './EmployeeDetail';
+import configureStore from '../store/configure-store';
+
+describe('<EmployeeDetail />', () => {
 // TODO
+});
