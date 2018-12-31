72ec5eb1a3be7f174bbc459dfe148d8b10b362bf
Mike Plummer <mike-plummer@users.noreply.github.com>
2018-12-31 13:54:27 -0600

    Updating labs

--- src/employees/EmployeeForm.js
+++ src/employees/EmployeeForm.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class EmployeeForm extends React.Component {
 // TODO
+}
+
+EmployeeForm.propTypes = {
+  employee: PropTypes.object,
+  handleSave: PropTypes.func
+};
+
+export default EmployeeForm;
