ee7dbb02ba62f1ee7079af48bdd9a9cbb8cb7654
Mike Plummer <mike-plummer@users.noreply.github.com>
2018-12-31 14:32:35 -0600

    Updating labs

--- src/employees/EmployeeDetail.js
+++ src/employees/EmployeeDetail.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import * as EmployeeActionCreators from '../actions/EmployeeActionCreator';
 import EmployeeForm from './EmployeeForm';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
 
 class EmployeeDetail extends React.Component {
   handleSave = (values) => {
