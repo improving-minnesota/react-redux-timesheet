d5f935c081acd05e4ccff63d0d2c36566bc98568
Anthony D <dunahee@gmail.com>
2019-04-13 13:25:14 -0500

    navigation working with shallow testing

--- lab-03/src/nav/Navigation.js
+++ lab-03/src/nav/Navigation.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { Nav, Navbar, NavItem } from 'react-bootstrap'
 import PropTypes from 'prop-types';
 
 class Navigation extends React.Component {
+  propTypes = {
+    history: PropTypes.object.isRequired,
+    location: PropTypes.object.isRequired,
+    match: PropTypes.object.isRequired,
+  }
+
   render() {
-    return null;
+    return (
+      <Navbar>
+        <Navbar.Header>
+          <Navbar.Brand>
+            Timesheetz
+          </Navbar.Brand>
+        </Navbar.Header>
+
+        <Nav>
+          <NavItem>
+            <NavLink to='/employees'>Employees</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to='/projects'>Projects</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to='/timesheets'>Timesheets</NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar>
+    )
   }
 }
 
--- lab-03/src/nav/Navigation.test.js
+++ lab-03/src/nav/Navigation.test.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Navigation from './Navigation';
+import {NavLink} from 'react-router-dom';
 
 describe('<Navigation />', () => {
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallow(<Navigation />);
+    });
+    it('should instantiage the Navigation Component with three nav links', () => {
+        expect(wrapper).toHaveLength(1);
+        expect(wrapper.find(NavLink)).toHaveLength(3);
+    });
+
 });
