130703c444b6dcb3743ef204c15d8dbbb1b9975e
Anthony D <dunahee@gmail.com>
2019-04-13 12:54:54 -0500

    router and switch set up

--- lab-03/src/App.js
+++ lab-03/src/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
 import Projects from "./projects/Projects";
 import Employees from './employees/Employees';
 import Timesheets from './timesheets/Timesheets';
+import Navigation from './nav/Navigation';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <div className="container">
-          <Projects />
+      <BrowserRouter>
+        <div className="App">
+          <div className="container">
+          <Navigation />
+            <Switch>
+              <Route exact path='/projects' component={Projects} />
+              <Route exact path='/employees' component={Employees} />
+              <Route exact path='/timesheets' component={Timesheets} />
+              <Redirect to='/employees' />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </BrowserRouter>
     );
   }
 }
